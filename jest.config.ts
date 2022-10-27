60e3b5bbc67dc200aaa49cb67584d49a0c3dc2e9
v-aguiar <v.aguiar.dev@gmail.com>
2022-10-27 14:17:50 -0300

    config: add initial dependencies and folder structure

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testMatch: ["<rootDir>/tests/**/*.(test|spec).ts"],
+  setupFiles: ["<rootDir>/tests/setup-envs.ts"],
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+    "@test/(.*)": "<rootDir>/tests/$1",
+  },
+  restoreMocks: true,
+};
+
+export default config;
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import "express-async-errors";
+import express, { Express } from "express";
+import cors from "cors";
+
+import { connectDb, disconnectDB } from "@/config";
+
+const app = express();
+app
+  .use(cors())
+  .use(express.json())
+  .get("/", (_req, res) => res.send("OK!"));
+
+export function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+  await disconnectDB();
+}
+
+export default app;
